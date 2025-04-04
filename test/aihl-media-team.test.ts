763e8aeed2bd77ce2bee781e65a61a9293b7298b
Brandyn Britton <Brandynbb96@gmail.com>
2025-04-04 21:23:56 +0800

    Initial commit

---
+++ test/aihl-media-team.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AihlMediaTeam from '../lib/aihl-media-team-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aihl-media-team-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AihlMediaTeam.AihlMediaTeamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
